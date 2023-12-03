52c2726421e3cf9e89db6e29089491f696a5a566
iamejcore <77796467+iamejcore@users.noreply.github.com>
2023-12-03 21:39:07 +0000

    Update time.c

--- time.c
+++ time.c
@@ -3,5 +3,5 @@
 double elapsed_time(struct timeval* start_time, struct timeval* end_time){
     //TODO: return the difference between end_time and start_time.
     double startsec = (start_time->tv_sec * 1000000.0)  + start_time->tv_usec, 
-    
+    endsec = (end_time->tv_sec * 1000000.0) + end_time->tv_usec;
 }
